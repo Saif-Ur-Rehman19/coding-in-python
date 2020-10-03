e9d9fad9581f46ee68b3a9a8dbdb2564cb287ac2
Saif Rehman <58853106+Saif-Ur-Rehman19@users.noreply.github.com>
2020-10-03 15:29:14 +0500

    Add files via upload

---
+++ Inserortion sort.py
@@ -0,0 +1,10 @@
+array = [3,2,6,0,1]
+for j in range(1,len(array)):
+    key = array[j]
+    i = j-1
+    while i >= 0 and array[i] > key:
+        array[i+1] = array[i]
+        i = i - 1
+    array[i+1] = key
+print(array)
+
